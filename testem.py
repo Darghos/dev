c1decb4eeae49458a454103d8e58e344c0eb1ec4
Marcus <129580156+Darghos@users.noreply.github.com>
2023-10-13 13:59:40 +0800

    Create testem.py

---
+++ testem.py
@@ -0,0 +1,15 @@
+import os
+list=[]
+directory_in_str="C:\Users\cdg0000151\Downloads\emails"
+directory = os.fsencode(directory_in_str)
+    
+for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     readfile=open(filename, "rt")
+     contents=readfile.read()
+     for line in contents:
+	     if 'Delivery has failed to' in line:
+    	   contents.readline()
+    	   lol=contents.readline()
+    	   list.append(lol)
+         break
