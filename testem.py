7ff7337c287b4f77465542e43df1ba55623b9f9e
Marcus <129580156+Darghos@users.noreply.github.com>
2023-10-13 14:10:14 +0800

    Update testem.py

--- testem.py
+++ testem.py
@@ -1,15 +1,20 @@
 import os
 list=[]
-directory_in_str="C:\Users\151\Downloads\emails"
+directory_in_str="C:\Users\cdg0000151\Downloads\emails"
 directory = os.fsencode(directory_in_str)
     
 for file in os.listdir(directory):
      filename = os.fsdecode(file)
-     readfile=open(filename, "rt")
-     contents=readfile.read()
+     readfile = open(filename, "rt")
+     contents = readfile.read()
      for line in contents:
-	     if 'Delivery has failed to' in line:
-    	   contents.readline()
+	if 'Delivery has failed to these recipients or groups' in line:
+    	   skip=contents.readline()
     	   lol=contents.readline()
     	   list.append(lol)
-         break
+           break
+		
+for i in list:
+   writefile=open('results.txt','w')
+   writefile.writeline(i)
+wriefile.close()
